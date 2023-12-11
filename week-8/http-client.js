87cba467249c7e1334ca677b577a0247e9ba05d0
Mackenzie Ortiz <mlubben1@gmail.com>
2023-12-10 20:08:38 -0500

    Add files via upload

---
+++ week-8/http-client.js
@@ -0,0 +1,20 @@
+// Title: http-client.js
+// Author: Mackenzie Lubben-Ortiz
+// Date: 12/9/2023
+// Description: WhataBook, Part 2
+
+class HttpClient {
+
+   async get(url, params) {
+    params = ''
+    return get
+
+    let url = newURL(url) 
+    let UrlSearchParams = new URLSearchParams(params.url.search.property)
+    const res = await fetch(url.toString(), {
+    method: "GET",
+   })
+
+}}
+
+export class HttpClient{}
